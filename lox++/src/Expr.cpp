c2fcf0e4554df31e91dd4e530acbcd56cd4bb3dd
HarithF <al-harith.farhad@uni-goettingen.de>
2026-04-10 12:57:41 +0200

    Finishing visitor pattern to implement functions.
    Forgot to push yesterday night. Implemented visitor instead of variant
    to stay closer to the book instead of idiomatic cpp

--- lox++/src/Expr.cpp
+++ lox++/src/Expr.cpp
@@ -2,37 +2,68 @@
 #include "token.h"
 #include <memory>
 
+struct Binary;
+struct Grouping;
+struct Literal;
+struct Unary;
+
+struct ExprVisitor {
+	virtual LiteralValue visit(Binary&) = 0;
+	virtual LiteralValue visit(Grouping&) = 0;
+	virtual LiteralValue visit(Literal&) = 0;
+	virtual LiteralValue visit(Unary&) = 0;
+	virtual ~ExprVisitor() = default;
+};
+
 struct Expr {
-  virtual ~Expr() = default;
+	virtual ~Expr() = default;
+	virtual LiteralValue accept(ExprVisitor&) = 0;
 };
 
-struct Binary : public Expr {
-  std::unique_ptr<Expr> left;
+struct Binary : public  Expr {
+	std::unique_ptr<Expr> left;
 
-  Token operator_;
+	Token operator_;
 
-  std::unique_ptr<Expr> right;
+	std::unique_ptr<Expr> right;
 
-  Binary(std::unique_ptr<Expr> left, Token operator_,
-         std::unique_ptr<Expr> right)
-      : left(std::move(left)), operator_(operator_), right(std::move(right)) {}
+
+	Binary(std::unique_ptr<Expr> left, Token operator_, std::unique_ptr<Expr> right)
+		: left(std::move(left)), operator_(operator_), right(std::move(right)) {}
+	LiteralValue accept(ExprVisitor& visitor) override {
+		return visitor.visit(*this);
+	}
 };
-struct Grouping : public Expr {
-  std::unique_ptr<Expr> expression;
+struct Grouping : public  Expr {
+	std::unique_ptr<Expr> expression;
 
-  Grouping(std::unique_ptr<Expr> expression)
-      : expression(std::move(expression)) {}
+
+	Grouping(std::unique_ptr<Expr> expression)
+		: expression(std::move(expression)) {}
+	LiteralValue accept(ExprVisitor& visitor) override {
+		return visitor.visit(*this);
+	}
 };
-struct Literal : public Expr {
-  LiteralValue value;
+struct Literal : public  Expr {
+	LiteralValue value;
 
-  Literal(LiteralValue value) : value(value) {}
+
+	Literal(LiteralValue value)
+		: value(std::move(value)) {}
+	LiteralValue accept(ExprVisitor& visitor) override {
+		return visitor.visit(*this);
+	}
 };
-struct Unary : public Expr {
-  Token operator_;
+struct Unary : public  Expr {
+	Token operator_;
 
-  std::unique_ptr<Expr> right;
+	std::unique_ptr<Expr> right;
 
-  Unary(Token operator_, std::unique_ptr<Expr> right)
-      : operator_(operator_), right(std::move(right)) {}
+
+	Unary(Token operator_, std::unique_ptr<Expr> right)
+		: operator_(operator_), right(std::move(right)) {}
+	LiteralValue accept(ExprVisitor& visitor) override {
+		return visitor.visit(*this);
+	}
 };
+
--- lox++/tools/define_ast.ml
+++ lox++/tools/define_ast.ml
@@ -1,22 +1,52 @@
 let newline ch = 
   output_char ch '\n'
 
+let define_base_struct ch super = 
+  output_string ch ("struct " ^ super ^ " {\n");
+  output_string ch ("\tvirtual ~" ^ super ^ "() = default;\n");
+  output_string ch ("\tvirtual LiteralValue accept(" ^ super ^ "Visitor&) = 0;\n");
+  output_string ch ("};\n\n")
 
 let define_constructor ch classname fields =
-    output_string ch ("\t" ^ classname ^ "(");
-    output_string ch fields;
-    output_string ch ")\n\t\t: ";
-    List.iter (fun fld ->
-        match String.split_on_char ' ' (String.trim fld) with
-        [typ ; name] -> 
-            if typ = "std::unique_ptr<Expr>" 
-              then  output_string ch (name ^ "(std::move(" ^ name ^ ")), ") 
-              else output_string ch (name ^ "(" ^ name ^ "), ")
-        | _ -> raise (Invalid_argument "bad sub-class")
-      )
-      (String.split_on_char ',' fields);
 
-    output_string ch " {}\n"
+  let inits =
+    List.map (fun fld ->
+      match String.split_on_char ' ' (String.trim fld) with
+      | [typ; name] ->
+          if typ = "std::unique_ptr<Expr>" || typ = "LiteralValue"
+          then name ^ "(std::move(" ^ name ^ "))"
+          else name ^ "(" ^ name ^ ")"
+      | _ -> raise (Invalid_argument "bad sub-class")
+    ) (String.split_on_char ',' fields)
+  in
+
+  output_string ch ("\t" ^ classname ^ "(");
+  output_string ch fields;
+  output_string ch ")\n\t\t: ";
+  output_string ch (String.concat ", " inits);
+  output_string ch " {}\n"
+
+let define_visitor ch super classes =
+    let names = List.map (fun cls ->
+        match String.split_on_char '-' cls with
+        | [name; _] -> String.trim name
+        | _ -> ""
+    ) classes in
+    output_string ch ("struct " ^ super ^ "Visitor {\n");
+    List.iter (fun name ->
+        output_string ch ("\tvirtual LiteralValue visit(" ^ name ^ "&) = 0;\n")
+    ) names;
+    output_string ch ("\tvirtual ~" ^ super ^ "Visitor() = default;\n};\n\n")
+
+let define_forward_decls ch classes =
+    List.iter (fun cls ->
+        match String.split_on_char '-' cls with
+        | [name; _] ->
+            output_string ch ("struct " ^ String.trim name ^ ";\n")
+        | _ -> ()
+    ) classes;
+    output_char ch '\n'
+
 
 let define_type ch super classinfo = 
   match String.split_on_char '-' classinfo with
@@ -35,16 +65,21 @@ let define_type ch super classinfo =
       (String.split_on_char ',' fields);
       newline ch;
       define_constructor ch classname fields;
-      output_string ch "};\n"
+      output_string ch ("\tLiteralValue accept(" ^ super ^ "Visitor& visitor) override {\n");
+      output_string ch ("\t\treturn visitor.visit(*this);\n");
+      output_string ch ("\t}\n");
+      output_string ch "};\n";
+
 
 | _ -> raise (Invalid_argument "bad class format")
   
 
 let define_ast ch super classes = 
   output_string ch ("#pragma once\n#include \"token.h\"\n#include <memory>\n\n");
-output_string ch ("struct " ^ super ^ " {\n\tvirtual ~" ^ super ^ "() = default;\n};\n\n");
+  define_forward_decls ch classes;
+  define_visitor ch super classes;
+  define_base_struct ch super;
   List.iter (define_type ch super) classes;
-
   newline ch
 
 
