cde344bb1350e3bc8ea42195c5d6b75e7aaa7bbb
HarithF <al-harith.farhad@uni-goettingen.de>
2026-07-30 22:03:04 +0200

    Start work on parser infrustructure
    Parser typedef start and error handling

--- clox/include/compiler.h
+++ clox/include/compiler.h
@@ -1,8 +1,9 @@
 #ifndef clox_compiler_h
 #define clox_compiler_h
 
+#include "chunk.h"
 #include "vm.h"
 
-void compile(const char *source, VM *vm);
+bool compile(const char *source, Chunk *chunk);
 
 #endif
